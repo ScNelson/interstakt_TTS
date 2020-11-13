5c09ea84c3d058e8621f535056068ea089ba1363
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-13 12:54:02 -0500

    init commit

---
+++ src/main/java/com/interstakt/interstaktweb/InterstaktwebApplication.java
@@ -0,0 +1,13 @@
+package com.interstakt.interstaktweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterstaktwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterstaktwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interstakt/interstaktweb/InterstaktwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interstakt.interstaktweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterstaktwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
