4db4bebb1fce0355c8966ef2ac8b0183d72560dd
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-13 13:12:06 -0500

    init from TTS Twitter Day 1, slide 23

---
+++ src/main/java/com/interstakt/interstaktweb/model/Role.java
@@ -0,0 +1,16 @@
+package com.interstakt.interstaktweb.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+    private String role;
+}
