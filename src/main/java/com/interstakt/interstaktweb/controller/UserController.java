b0696fd4d4612dbe3b8e8ef6459d30bf91b9cb34
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-13 15:35:15 -0500

    Added @Controller and changed mapping value

--- src/main/java/com/interstakt/interstaktweb/controller/UserController.java
+++ src/main/java/com/interstakt/interstaktweb/controller/UserController.java
@@ -8,10 +8,12 @@ import com.interstakt.interstaktweb.service.ScoreService;
 import com.interstakt.interstaktweb.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class UserController {
     @Autowired
     private UserService userService;
@@ -19,13 +21,13 @@ public class UserController {
     @Autowired
     private ScoreService scoreService;
 
-    @GetMapping(value = "/users/{username}")
+    @GetMapping(value = "/profile/{username}")
     public String getUser(@PathVariable(value = "username") String username, Model model) {
         User user = userService.findByUsername(username);
-        List<Score> tweets = scoreService.findAllByUser(user);
-        model.addAttribute("tweetList", tweets);
+        List<Score> scores = scoreService.findAllByUser(user);
+        model.addAttribute("scoreList", scores);
         model.addAttribute("user", user);
-        return "user";
+        return "profile";
     }
 
 }
