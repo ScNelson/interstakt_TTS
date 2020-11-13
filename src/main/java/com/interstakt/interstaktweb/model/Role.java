0c11231bceefb62ac9b883cac99b0baa27579714
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-13 13:34:40 -0500

    Add constructors, getters/setters, toString

--- src/main/java/com/interstakt/interstaktweb/model/Role.java
+++ src/main/java/com/interstakt/interstaktweb/model/Role.java
@@ -12,5 +12,25 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "role_id")
     private Long id;
+    
     private String role;
+
+    public Role() {}
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [role=" + role + "]";
+    }   
 }
