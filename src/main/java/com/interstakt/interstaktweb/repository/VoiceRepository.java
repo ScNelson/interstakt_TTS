7afc05107b88eac367e53a2112046710644cc339
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-14 02:34:40 -0500

    init commit

---
+++ src/main/java/com/interstakt/interstaktweb/repository/VoiceRepository.java
@@ -0,0 +1,22 @@
+package com.interstakt.interstaktweb.repository;
+
+import java.util.List;
+
+import com.interstakt.interstaktweb.model.Score;
+import com.interstakt.interstaktweb.model.User;
+import com.interstakt.interstaktweb.model.Voice;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface VoiceRepository extends CrudRepository<Voice, Long>{
+    Voice findByName(String name);
+
+    List<Voice> findAllByOrderByCreatedAtDesc();
+
+    List<Voice> findAllByUserOrderByCreatedAtDesc(User user);
+
+    List<Voice> findAllByScoreOrderByCreatedAtDesc(Score score);
+}
