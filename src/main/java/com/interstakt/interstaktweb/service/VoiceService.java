a96a043215ed3fe4d71151fa454ad0006174d6db
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-14 02:34:59 -0500

    init commit

---
+++ src/main/java/com/interstakt/interstaktweb/service/VoiceService.java
@@ -0,0 +1,45 @@
+package com.interstakt.interstaktweb.service;
+
+import java.util.List;
+
+import com.interstakt.interstaktweb.model.Score;
+import com.interstakt.interstaktweb.model.User;
+import com.interstakt.interstaktweb.model.Voice;
+import com.interstakt.interstaktweb.repository.VoiceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VoiceService {
+    @Autowired
+    private VoiceRepository voiceRepository;
+
+    public Voice find(String name) {
+        Voice voice = voiceRepository.findByName(name);
+        return voice;
+    }
+
+    public List<Voice> findAll() {
+        List<Voice> scores = voiceRepository.findAllByOrderByCreatedAtDesc();
+        return scores;
+    }
+
+    public List<Voice> findAllByUser(User user) {
+        List<Voice> voices = voiceRepository.findAllByUserOrderByCreatedAtDesc(user);
+        return voices;
+    }
+
+    public List<Voice> findAllByScore(Score score) {
+        List<Voice> voices = voiceRepository.findAllByScoreOrderByCreatedAtDesc(score);
+        return voices;
+    }
+
+    public void save(Voice voice) {
+        voiceRepository.save(voice);
+    }
+
+    public void delete(long id) {
+        voiceRepository.deleteById(id);
+    }
+}
