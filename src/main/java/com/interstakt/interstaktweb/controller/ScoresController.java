c9d13612f5b6f567733443a0d273d82292b9fd26
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-14 00:55:25 -0500

    init from Score Controller

---
+++ src/main/java/com/interstakt/interstaktweb/controller/ScoresController.java
@@ -0,0 +1,78 @@
+package com.interstakt.interstaktweb.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.interstakt.interstaktweb.model.Score;
+import com.interstakt.interstaktweb.model.User;
+import com.interstakt.interstaktweb.service.ScoreService;
+import com.interstakt.interstaktweb.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ScoresController {
+    @Autowired
+    private UserService userService;
+	
+    @Autowired
+    private ScoreService scoreService;
+
+    public void addScoresAttributes(Model model){
+        List<Score> scores = scoreService.findAll();
+        Score score = new Score();
+        User user = userService.getLoggedInUser();
+        model.addAttribute("scoreList", scores);
+        model.addAttribute("score", score);
+        model.addAttribute("user", user);
+    }
+    
+    @GetMapping(value= {"/scores"})
+    public String getScores(Model model){
+        addScoresAttributes(model);
+        return "scores";
+    }
+
+    @GetMapping(value= {"/score/{title}"})
+    public String getScoreWithID(@PathVariable String title, Model model){
+        User user = userService.getLoggedInUser();
+        Score score = scoreService.find(title);
+        model.addAttribute("user", user);
+        model.addAttribute("score", score);
+        return "score";
+    }
+    
+    @GetMapping(value = "/scores/new")
+    public String getScoreForm(Model model) {
+        model.addAttribute("score", new Score());
+        return "newScore";
+    }
+    
+    @PostMapping(value = "/scores")
+    public String submitScoreForm(@Valid Score score, BindingResult bindingResult, Model model) {
+        User user = userService.getLoggedInUser();
+        if (!bindingResult.hasErrors()) {
+            score.setUser(user);
+            scoreService.save(score);
+            model.addAttribute("successMessage", "Score successfully created!");
+            addScoresAttributes(model);
+        }
+        return "scores";
+    }
+
+    @RequestMapping(value = "/scores/delete/{id}")
+    public String deletePostWithID(@PathVariable Long id, Score score, Model model) {
+        scoreService.delete(id);
+        addScoresAttributes(model);
+        
+        return "scores";
+    }
+}
