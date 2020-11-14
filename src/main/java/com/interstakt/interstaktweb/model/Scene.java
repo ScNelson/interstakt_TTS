1b777c1a2183a4fc898101257cc0b6d82f761929
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-14 14:13:52 -0500

    Add Score, User, Voice attributes

--- src/main/java/com/interstakt/interstaktweb/model/Scene.java
+++ src/main/java/com/interstakt/interstaktweb/model/Scene.java
@@ -2,9 +2,15 @@ package com.interstakt.interstaktweb.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 public class Scene {
@@ -12,4 +18,19 @@ public class Scene {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "scene_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "score_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+    private Score score;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "voice_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+    private Voice voice;
 }
